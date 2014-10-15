1d5c311192522ea35081ed55a450d6c3e7ac95cc
Lou Parslow <lou.parslow@gmail.com>
2014-10-15 16:59:18 -0600

    commit all

---
+++ CopyHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace JsonNet
+{
+    public static class CopyHelper
+    {
+        public static void Copy(object source,object destination)
+        {
+            System.Collections.IEnumerable sourceEnumerable = source as System.Collections.IEnumerable;
+            System.Collections.IList destinationList = destination as System.Collections.IList;
+            if(!object.ReferenceEquals(null,sourceEnumerable) &&
+               !object.ReferenceEquals(null,destinationList))
+            {
+                System.Collections.IEnumerator denum = sourceEnumerable.GetEnumerator();
+                while(denum.MoveNext())
+                {
+
+                }
+            }
+        }
+    }
+}
